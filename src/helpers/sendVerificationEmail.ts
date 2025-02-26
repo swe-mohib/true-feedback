ee987ac2b3a51837e353eccf44468b365cadaa2f
Mohd Mohibuddin <swe.mohib@gmail.com>
2025-02-26 09:18:19 +0530

    feat: setup for sending email

---
+++ src/helpers/sendVerificationEmail.ts
@@ -0,0 +1,28 @@
+import { ApiResponse } from "@/types/ApiResponse";
+import { resend } from "@/lib/resend";
+import VerificationEmail from "../../emails/VerificationEmail";
+
+export async function sendVerificationEmail(
+  email: string,
+  username: string,
+  verificationCode: string
+): Promise<ApiResponse> {
+  try {
+    await resend.emails.send({
+      from: "support@mohibs.site",
+      to: email,
+      subject: "True-Feedback verification code",
+      react: VerificationEmail({ username, otp: verificationCode }),
+    });
+    return {
+      success: true,
+      message: "Verification email sent to your email successfullly.",
+    };
+  } catch (emailError) {
+    console.error("Error sending verification email:", emailError);
+    return {
+      success: false,
+      message: "Failed to send verification email.",
+    };
+  }
+}
