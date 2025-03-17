2feae8066063ef0e014f38e62796801725a9220b
Mohd Mohibuddin <swe.mohib@gmail.com>
2025-03-17 20:45:52 +0530

    chore: Disables type checking and eslint during build

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  typescript: {
+    // !! WARN !!
+    // Disables type checking during build (use with caution)
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    // !! WARN !!
+    // Disables ESLint during build (use cautiously)
+    ignoreDuringBuilds: true,
+  },
 };
 
 export default nextConfig;
